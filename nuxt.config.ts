790b9054f40b8b944052fa0a3156ca2b6751806b
Dogukan <dogukan.cirpan@apave.com>
2023-10-24 09:36:21 +0200

    :package: install vuetfiy, vite

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/color-mode'],
+  modules: [
+    '@nuxtjs/color-mode',
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },
+  ],
+  build: {
+    transpile: ['vuetify'],
+  },
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
   $development: {
     routeRules: {
       '/': { prerender: true }
     }
-  }
+  },
 })
