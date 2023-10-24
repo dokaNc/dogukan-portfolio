73813c998f74a4dcc48d7ed5b24752b523663259
Dogukan <dogukan.cirpan@apave.com>
2023-10-24 14:41:02 +0200

    :sparkles: ChipDefault component created

---
+++ components/chip/ChipDefault.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-chip class="px-6" variant="outlined" :class="{ active: active }">
+    {{ props.text }}
+  </v-chip>
+</template>
+
+<script setup>
+const props = defineProps({
+  text: {
+    type: String,
+  },
+  active: {
+    type: Boolean,
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.v-chip {
+  color: $light-grey-main;
+  transition: all 0.4s;
+
+  &.v-chip--variant-outlined {
+    border-style: dashed;
+  }
+
+  &.active {
+    color: $blue-main;
+    font-weight: 500;
+    transition: all 0.4s;
+  }
+}
+
+// Dark Mode
+.v-theme--darkTheme {
+  .v-chip {
+    color: $dark-grey-main;
+
+    &.active {
+      color: $blue-main;
+      font-weight: 500;
+      transition: all 0.4s;
+    }
+  }
+}
+</style>
