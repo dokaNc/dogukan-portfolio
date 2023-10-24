a1d24eda10efc9d9e8ebf4272a8f5649f271a64b
Dogukan <dogukan.cirpan@apave.com>
2023-10-24 11:23:02 +0200

    :package: setup eslint, prettier

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,29 +2,29 @@
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: [
-    '@nuxtjs/color-mode',
-    (_options, nuxt) => {
-      nuxt.hooks.hook('vite:extendConfig', (config) => {
-        // @ts-expect-error
-        config.plugins.push(vuetify({ autoImport: true }))
-      })
+    devtools: { enabled: true },
+    modules: [
+        '@nuxtjs/color-mode',
+        (_options, nuxt) => {
+            nuxt.hooks.hook('vite:extendConfig', (config) => {
+                // @ts-expect-error
+                config.plugins.push(vuetify({ autoImport: true }))
+            })
+        },
+    ],
+    build: {
+        transpile: ['vuetify'],
     },
-  ],
-  build: {
-    transpile: ['vuetify'],
-  },
-  vite: {
-    vue: {
-      template: {
-        transformAssetUrls,
-      },
+    vite: {
+        vue: {
+            template: {
+                transformAssetUrls,
+            },
+        },
     },
-  },
-  $development: {
-    routeRules: {
-      '/': { prerender: true }
-    }
-  },
+    $development: {
+        routeRules: {
+            '/': { prerender: true },
+        },
+    },
 })
