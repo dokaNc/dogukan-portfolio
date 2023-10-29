e4ed32ac09eaabdaa7875502ce8e64f8a4f4b8b1
Dogukan <dogukan.cirpan@apave.com>
2023-10-29 02:50:54 +0200

    :lipstick: init dark mode vuetify

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,14 +1,35 @@
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
+import { createVuetify, ThemeDefinition } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const darkTheme: ThemeDefinition = {
+  dark: true,
+  colors: {
+    background: "#292929",
+  },
+};
+
+const lightTheme: ThemeDefinition = {
+  dark: false,
+  colors: {
+    background: "#f2f5f8",
+  },
+};
 
 export default defineNuxtPlugin((app) => {
   const vuetify = createVuetify({
     ssr: true,
     components,
-    directives
-  })
-  app.vueApp.use(vuetify)
-})
+    directives,
+    theme: {
+      defaultTheme: "darkTheme",
+      themes: {
+        darkTheme,
+        lightTheme,
+      },
+    },
+  });
+  app.vueApp.use(vuetify);
+});
