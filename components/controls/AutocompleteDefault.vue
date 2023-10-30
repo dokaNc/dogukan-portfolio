1137e94773597a4877f0b2c110c7fff004bb1af9
Dogukan <dogukan.cirpan@apave.com>
2023-10-30 12:51:14 +0100

    :sparkles: Autocomplete component created

---
+++ components/controls/AutocompleteDefault.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-autocomplete
+    chips
+    hide-selected
+    clearable
+    :hide-details="true"
+    item-color="#000"
+    no-data-text="Aucune compétence de disponibles"
+    :label="props.label"
+    :items="props.data"
+    multiple
+    variant="underlined"
+    :model-value="props.selected"
+    @input="$emit('update:props.selected', $event.target.value)"
+  ></v-autocomplete>
+</template>
+
+<script setup>
+const props = defineProps({
+  label: {
+    type: String,
+  },
+  data: {
+    type: Array,
+  },
+  selected: {
+    type: String,
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.v-field__input {
+  padding-bottom: 8px !important;
+}
+
+.v-input__details {
+  display: none !important;
+}
+</style>
