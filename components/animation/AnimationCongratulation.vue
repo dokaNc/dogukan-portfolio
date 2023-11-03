db5990e8f90ff36fb102e2cd8dfe07106c7630aa
dokaNc <cirpan.dogukan5959@gmail.com>
2023-11-03 01:15:51 +0100

    :sparkles: add AnimationCongratulation comp

---
+++ components/animation/AnimationCongratulation.vue
@@ -0,0 +1,56 @@
+<template>
+  <client-only>
+    <Vue3Lottie :animationData="congratulationJSON" :width="350" />
+  </client-only>
+  <div class="congratulation">
+    <h1>IL Y’A MATCH !</h1>
+    <h2>
+      avec <span>{{ props.congratulationText }}</span>
+    </h2>
+  </div>
+</template>
+
+<script setup>
+import congratulationJSON from "./assets/lottiefiles/congratulation.json";
+
+const props = defineProps({
+  congratulationText: {
+    type: String,
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.lottie-animation-container {
+  animation: fadeOut 2s;
+}
+
+.congratulation {
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 100%;
+  line-height: 1;
+  animation: fadeOut 2s;
+
+  h1 {
+    font-size: 2.5rem;
+  }
+
+  h2 {
+    font-weight: 500;
+  }
+}
+
+// KEYFRAMES
+@keyframes fadeOut {
+  50% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+}
+</style>
